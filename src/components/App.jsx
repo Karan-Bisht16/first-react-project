1f477a056771347e84eec3a65c94c81f94752f76
Karan Bisht <karan161003@gmail.com>
2024-05-01 22:56:38 +0530

    modified:   src/components/App.jsx 	new file:   src/components/Footer.jsx 	modified:   src/details.js 	modified:   src/index.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Card from "./Card";
 import Avatar from "./Avatar";
+import Footer from "./Footer";
 import characterData from "../details";
 
 function App() {
     return (
-        <div className="main">
-            <div style={{ display: "flex", gap: "20px", justifyContent: "center", margin: "0 20px" }}>
-                <p style={{ fontSize: "48px", fontWeight: "bold", textAlign: "right" }}>Current status of Attack on Titan Characters</p>
-                <Avatar img="https://i.pinimg.com/originals/cb/65/82/cb65826a6472c3edbddc481e9de9c734.jpg" />
+        <div>
+            <div className="main">
+                <div className="heading">
+                    <p>Current status of Attack on Titan Characters</p>
+                    <Avatar img="https://i.pinimg.com/originals/cb/65/82/cb65826a6472c3edbddc481e9de9c734.jpg" />
+                </div>
+                <div style={{ display: "flex", gap: "20px", flexFlow: "row wrap" }}>
+                    {characterData.map(data => (
+                        <Card {...data} />
+                    ))}
+                </div>
             </div>
-            <div style={{ display: "flex", gap: "20px", flexFlow: "row wrap" }}>
-                {characterData.map(data => (
-                    <Card {...data} />
-                ))}
-            </div>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="custom-footer">
+            <div className="addn-info">
+                <h3 style={{ paddingBottom: "5px" }}> &nbsp; Created with ❤️</h3>
+                <h4>Thank you Isayama!</h4>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
