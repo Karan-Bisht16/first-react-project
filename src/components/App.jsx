1d97dc78cfa3701adcda37f6e7accb9a04f211ea
Karan Bisht <karan161003@gmail.com>
2024-05-03 15:12:06 +0530

    modified:   package.json 	modified:   src/components/App.jsx 	modified:   src/details.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,18 @@ import Avatar from "./Avatar";
 import Footer from "./Footer";
 import characterData from "../details";
 
+function createCards(data) {
+    return (
+        <Card 
+            key = {data.id}
+            id = {data.id}
+            name = {data.name}
+            content = {data.content}
+            img = {data.img}
+        />
+    );
+}
+
 function App() {
     return (
         <div>
@@ -13,9 +25,7 @@ function App() {
                     <Avatar img="https://i.pinimg.com/originals/cb/65/82/cb65826a6472c3edbddc481e9de9c734.jpg" />
                 </div>
                 <div style={{ display: "flex", gap: "20px", flexFlow: "row wrap" }}>
-                    {characterData.map(data => (
-                        <Card {...data} />
-                    ))}
+                    {characterData.map(createCards)}
                 </div>
             </div>
             <Footer />
--- src/details.js
+++ src/details.js
@@ -1,50 +1,58 @@
 const characterData = [
     {
+        id: 1,
         name: "Eren",
         content: "Dead ☠️",
         img: "https://static.wikia.nocookie.net/f56a5033-50e0-4e2a-9fe5-14eddb0889e4/scale-to-width/755"
     },
     {
+        id: 2,
         name: "Mikasa",
         content: "Sad 😥",
         img: "https://static.wikia.nocookie.net/shingekinokyojin/images/f/f7/Mikasa_Ackerman_character_image.png"
     },
     {
+        id: 3,
         name: "Armin",
         content: "Spreading +vity 🥰",
         img: "https://static.wikia.nocookie.net/shingekinokyojin/images/b/b2/Armin_Arlert_character_image_%28850%29.png"
     },
     {
+        id: 4,
         name: "Levi",
         content: "Old bad boy 👴",
         img: "https://static.wikia.nocookie.net/shingekinokyojin/images/f/f9/Levi_Ackerman_character_image_%28850%29.png"
     },
     {
+        id: 5,
         name: "Hange",
         content: "Crisp 🔥",
         img: "https://static.wikia.nocookie.net/shingekinokyojin/images/8/87/Hange_Zo%C3%AB_character_image.png"
     },
     {
+        id: 6,
         name: "Erwin",
         content: "Steel balls run 💪",
         img: "https://static.wikia.nocookie.net/shingekinokyojin/images/1/18/Erwin_Smith_character_image.png/"
     },
     {
+        id: 7,
         name: "Jean",
         content: "Livin’ The Life 🐴",
         img: "https://64.media.tumblr.com/1315919fdc54281de8f1112bb92a1ac6/f9a75d46c093de98-7c/s1280x1920/c81ee5b11536a9a950e3d1b24632d4b1b07703a9.jpg"
     },
     {
+        id: 8,
         name: "Reiner",
         content: "ISN't suicidal 😇",
         img: "https://static.wikia.nocookie.net/shingekinokyojin/images/5/53/Reiner_Braun_character_image_%28850%29.png"
     },
     {
+        id: 9,
         name: "Sasha",
         content: "Dead 🍖",
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9jvzWGmBsLwnBtMIC0OV_3GVngJgxnRCv_c4HCL3HrQ&s"
     }
-
 ];
 
 export default characterData;
